55ece9ac789e146c8537619a95007125468bc172
Sidra <smanzoor@andrew.cmu.edu>
2021-11-28 13:50:11 -0500

    Added Animation

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,12 @@
-.col
+AOS.init();
+
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     var projectImg = document.querySelector('.project-thumbnail');
+
+
+//     navItem.addEventListener('mouseenter', function (e) {
+//         projectImg.style
+//         projectImg.classList.add('animate__fadeInLeft');
+//     });
+// })
