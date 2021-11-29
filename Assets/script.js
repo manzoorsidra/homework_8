75cc19c7ed20738e9a71f42ff519f4e70c33c878
Sidra <smanzoor@andrew.cmu.edu>
2021-11-28 19:32:41 -0500

    About Page

--- Assets/script.js
+++ Assets/script.js
@@ -1,12 +1,1 @@
 AOS.init();
-
-
-// document.addEventListener('DOMContentLoaded', function () {
-//     var projectImg = document.querySelector('.project-thumbnail');
-
-
-//     navItem.addEventListener('mouseenter', function (e) {
-//         projectImg.style
-//         projectImg.classList.add('animate__fadeInLeft');
-//     });
-// })
